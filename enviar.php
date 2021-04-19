8064c5a1084618f51081342604695f926b2c862a
Daniel Araque <danielfparaque@gmail.com>
2021-04-19 09:28:30 -0500

    For9

--- enviar.php
+++ enviar.php
@@ -1,23 +1,22 @@
-<?php
+<?php  
 
 // Llamando a los campos
-$nombre = $_POST [ "nombre"];
-$nombre = $_POST [ "correo"];
-$nombre = $_POST [ "telefono"];
-$nombre = $_POST [ "mensaje"];
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
 
-
-// datos para el correo
-$destinatario = "danielfparaque@gmail.com"
-$asunto = "contacto desde nuestra web"
+// Datos para el correo
+$destinatario = "danielfparaque@gmail.com";
+$asunto = "Contacto desde nuestra web";
 
 $carta = "De: $nombre \n";
-$carta .= "correo: $correo \n";
-$carta .= "telefono: $telefono \n"
-$carta .= "mensaje: $mensaje";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
 
-//enviando mensaje
-$mail = mail($destinatario, $asunto, $carta);
-
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
 
 ?>
